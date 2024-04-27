322bdca633f1a8818ec41abd99eb596adeaea86c
Batty-sk <saorav.skumar@gmail.com>
2024-04-27 15:30:28 +0530

    added On mail and On data logic

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const SMTPServer = require('smtp-server').SMTPServer;
 
-const mailServer = new SMTPServer({
-    onData(stream, session,cb){
-        stream.pipe(process.stdout)
-        stream.on('end', callback);
-
-    }
-})
+    const mailServer = new SMTPServer({
+        onConnect(session, cb){
+            console.log('session ',session)
+            cb() // if we're passing a error object in it it means we are rejecting the request.
+        },
+        onMailFrom(address,session,cb){
+            console.log('Sender Mail Address: ',address.address)
+            cb()
+        },
+        onData(stream,session,cb){
+            console.log('coming data stream',stream)
+        }
+    })
 
 mailServer.listen(25,()=>{
     console.log('mail server has been started!')
